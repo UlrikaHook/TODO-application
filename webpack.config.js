d86cb7a01c54540093684e58f39390c9725f0a15
Ulrika Höök <hook.ulrika@gmail.com>
2020-06-11 09:32:42 +0200

    setup of development enviroment

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+
+    output: {
+        path: __dirname + '/dist', //Önskar förklaring på denna
+        filename: "bundle.js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
